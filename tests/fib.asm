46f844256e484153adcc64cebc79aea92b079394
ciscou <francismpp@gmail.com>
2022-01-21 11:29:22 +0100

    reimplement fib with while

--- tests/fib.asm
+++ tests/fib.asm
@@ -1,102 +1,25 @@
-1
+0
 store a
 1
 store b
+26
+store c
+load c
+0
+gt
+jz 23
+load a
+store d
 load a
 load b
 +
+store a
+load d
+store b
+load c
+1
+-
 store c
-load b
-load c
-+
-store d
-load c
-load d
-+
-store e
-load d
-load e
-+
-store f
-load e
-load f
-+
-store g
-load f
-load g
-+
-store h
-load g
-load h
-+
-store i
-load h
-load i
-+
-store j
-load i
-load j
-+
-store k
-load j
-load k
-+
-store l
-load k
-load l
-+
-store m
-load l
-load m
-+
-store n
-load m
-load n
-+
-store o
-load n
-load o
-+
-store p
-load o
-load p
-+
-store q
-load p
-load q
-+
-store r
-load q
-load r
-+
-store s
-load r
-load s
-+
-store t
-load s
-load t
-+
-store u
-load t
-load u
-+
-store v
-load u
-load v
-+
-store w
-load v
-load w
-+
-store x
-load w
-load x
-+
-store y
-load x
-load y
-+
-store z
-load z
+jmp 6
+load a
 writeln
--- tests/fib.pas
+++ tests/fib.pas
@@ -1,27 +1,10 @@
-a := 1;
+a := 0;
 b := 1;
-c := a + b;
-d := b + c;
-e := c + d;
-f := d + e;
-g := e + f;
-h := f + g;
-i := g + h;
-j := h + i;
-k := i + j;
-l := j + k;
-m := k + l;
-n := l + m;
-o := m + n;
-p := n + o;
-q := o + p;
-r := p + q;
-s := q + r;
-t := r + s;
-u := s + t;
-v := t + u;
-w := u + v;
-x := v + w;
-y := w + x;
-z := x + y;
-writeln(z)
+c := 26;
+while(c > 0)
+  d := a;
+  a := a + b;
+  b := d;
+  c := c - 1
+end;
+writeln(a)
